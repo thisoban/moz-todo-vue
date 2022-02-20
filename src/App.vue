a7a03b4de01fb596328c394dffdd582e9f7b694d
thisoban <thisoban_1995@hotmail.com>
2022-02-20 21:51:15 +0100

    tuturial vollowing

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1> To-Do List </h1>
+  <ul>
+    <li>
+      <ToDoItem label="My ToDo Item"></ToDoItem>
+    </li>
+  </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ToDoItem from './components/ToDoItem.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+ components:{
+   ToDoItem
+ }
 }
 </script>
 
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input type="checkbox" id="todo-item" />
+        <label for="todo-item">{{label}}</label>
+  </div>
+</template>
+<script>
+export default {
+ props:{ label:{required: true, type: String},
+         done: {default: false, type: Boolean}   
+ },
+ data(){
+return{
+        isDone: this.done
+    }   
+ }
+};
+</script>
